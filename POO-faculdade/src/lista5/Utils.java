752c0d22c24c0909a4e38db90a9fde730b215d4d
Gabriela <96033603+Gabrielasl18@users.noreply.github.com>
2022-12-01 13:34:09 -0300

    Update Utils.java

--- POO-faculdade/src/lista5/Utils.java
+++ POO-faculdade/src/lista5/Utils.java
@@ -1,7 +1,6 @@
 package lista5;
 
-import java.util.Comparator;
-import java.util.List;
+import java.util. *;
 
 public class Utils {
 	public static boolean existe(List<Aluno> x, Aluno y) {
@@ -10,4 +9,34 @@ public class Utils {
 	public static void ordena(List<Aluno> x) {
 		x.sort(Comparator.comparing(Aluno::getNome));
 	}
-}
+	public Map<String, Aluno> retornaDados(Set conjuntoAlunos) {
+		Map<String,Aluno> mapaAlunos = new HashMap<String,Aluno>();
+		Iterator it = conjuntoAlunos.iterator();
+
+		while(it.hasNext()) {
+			String k = it.next().toString();
+			System.out.println(k);
+			String arrayK [] = k.split("#");
+			if(arrayK[1].equals("E")) {
+				AlunoEspecial alunoE = new AlunoEspecial(arrayK[0]);
+				System.out.println("nome: ...");
+				System.out.println("notas: ...");
+				alunoE.setNota(7);
+				mapaAlunos.put(arrayK[0], alunoE);
+				System.out.println("aluno E criado.");
+			}
+			else if(arrayK[1].equals("R")){
+				AlunoRegular alunoR = new AlunoRegular(arrayK[0]);
+				System.out.println("nome: ...");
+				System.out.println("notas: ...");
+				alunoR.setNota(8); 
+				mapaAlunos.put(arrayK[0], alunoR);
+				System.out.println("aluno R criado.");
+			}
+			else {
+				
+			}
+		}
+		return mapaAlunos;
+	}
+}	
